bebe495035fac8ba34ff05384bc3238cca7bebf3
Irina Antonets <i.antonets@spheremall.com>
2019-11-20 16:55:05 +0200

    Add router updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'awesome-app';
-  activeTab: string = 'recipes';
-
-  onNavigate(tabName: string) {
-    this.activeTab = tabName;
-  }
 }

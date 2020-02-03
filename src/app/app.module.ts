b4a86e33aff3b8e0a526383397fd40bf16708bb0
user <user@users-MacBook-Pro-2.local>
2020-02-03 09:58:24 +0200

    Add reactive forms example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 /* Application components */
@@ -74,6 +74,7 @@ import { RecipeEditComponent } from './components/recipes/recipe-edit/recipe-edi
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [RecipeService, ShoppingListService],

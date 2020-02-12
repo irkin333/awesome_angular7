528963f4076fb495da7b7dbc3c69b0bd12e90f36
irkin333 <airkin2@gmail.com>
2020-02-12 06:27:21 +0200

    Add example for http from the course

--- src/app/assignments/awesome-https/awesome-https.component.ts
+++ src/app/assignments/awesome-https/awesome-https.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-awesome-https',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./awesome-https.component.scss']
 })
 export class AwesomeHttpsComponent implements OnInit {
+  loadedPosts = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http
+      .post(
+        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
   }
 
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
 }
--- src/app/module-imports/ng-core-imports.ts
+++ src/app/module-imports/ng-core-imports.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 export const NgCoreImports = [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule
 ]

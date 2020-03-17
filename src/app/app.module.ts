a417fe1edeb75a0ea159783ca15c806b756e147a
irkin333 <airkin2@gmail.com>
2020-03-17 06:32:26 +0200

    Add animations to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,25 @@ import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { AppComponent } from './app.component';
 
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AuthComponent } from './auth/auth.component';
-import { ErrorPageComponent } from './components/error-page/error-page.component';
-import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import * as fromApp from './store/app.reducers';
+import { environment } from 'src/environments/environment';
 import { AuthEffects } from './auth/store/auth.effects';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -38,7 +39,8 @@ import { environment } from 'src/environments/environment';
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
     StoreDevtoolsModule.instrument({logOnly: environment.production}),
-    StoreRouterConnectingModule.forRoot()
+    StoreRouterConnectingModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [{ 
     provide: HTTP_INTERCEPTORS,
